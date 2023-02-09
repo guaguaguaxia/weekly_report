3e37c4524e2fe037ee7a92f5a9393a80744a3563
zjzj1996 <guaguaguaxia@gmail.com>
2023-02-09 15:49:38 +0800

    1

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,7 @@ import { useTranslations } from 'next-intl'
 import { Toaster, toast } from "react-hot-toast";
 import DropDown, { FormType } from "../components/DropDown";
 import Footer from "../components/Footer";
+import Github from "../components/GitHub";
 
 import Header from "../components/Header";
 import LoadingDots from "../components/LoadingDots";
@@ -92,6 +93,15 @@ const Home: NextPage = () => {
 
       <Header />
       <main className="flex flex-1 w-full flex-col items-center justify-center text-center px-4 mt-12 sm:mt-20">
+      <a
+          className="flex max-w-fit items-center justify-center space-x-2 rounded-full border border-gray-300 bg-white px-4 py-2 text-sm text-gray-600 shadow-md transition-colors hover:bg-gray-100 mb-5"
+          href="https://github.com/guaguaguaxia/weekly_report"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Github />
+          <p>Star on GitHub</p>
+        </a>
         <h1 className="sm:text-6xl text-4xl max-w-2xl font-bold text-slate-900">
           {t('description1')} <br></br>{t('description2')}
         </h1>
---
+++ privacy.md
@@ -0,0 +1,5 @@
+## 隐私声明
+我们重视您的隐私。该网站不存储您上传的任何内容。所有内容将被直接上传到OpenAI，OpenAI将承担所有与该内容相关的责任。该网站不对任何与上传内容有关的法律责任负责。请确保您了解[OpenAI的隐私政策](https://openai.com/privacy/)，并同意其所涵盖的内容。
+
+## Privacy Statement
+Your privacy is important to us. This site does not store any content you upload. All content will be uploaded directly to OpenAI and OpenAI will assume all responsibility in relation to that content. The site is not responsible for any legal liability related to the uploaded content. Please ensure that you understand [OpenAI's Privacy Policy](https://openai.com/privacy/) and agree to the content covered by it.
