916a31ded68c19159a298e0c5c62c237adc78d9d
zkl <i@zkl2333.com>
2023-02-10 15:16:01 +0800

    feat: 🎸 结果用 md 渲染

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import { Analytics } from "@vercel/analytics/react";
 import type { AppProps } from "next/app";
 import { NextIntlProvider } from 'next-intl'
 import "../styles/globals.css";
+import "../styles/markdown.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
