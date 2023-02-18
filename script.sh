2fe3297616896c6be8bd595d92d80f268f6ca798
zjzj1996 <guaguaguaxia@gmail.com>
2023-02-18 22:02:44 +0800

    feat:stop auto deploy

---
+++ script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [[ "$VERCEL_GIT_COMMIT_REF" == "main"  ]] ; then
+  # Don't build
+  echo "🛑 - Build cancelled"
+  exit 0;
+
+else
+  # Proceed with the build
+    echo "✅ - Build can proceed"
+  exit 1;
+fi
