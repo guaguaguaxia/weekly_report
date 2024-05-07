3cf92dea2a416c31acf3d1a7fa5a09eca25ef774
zjzj1996 <guaguaguaxia@Gmail.com>
2024-05-07 11:38:23 +0800

    feat:替换为转发接口

--- utils/OpenAIStream.ts
+++ utils/OpenAIStream.ts
@@ -54,7 +54,7 @@ export async function OpenAIStream(payload: OpenAIStreamPayload) {
   }
   delete payload.api_key
 
-  const res = await fetch("https://api.openai.com/v1/chat/completions", {
+  const res = await fetch("https://gtapi.xiaoerchaoren.com:8932/v1/chat/completions", {
     headers: {
       "Content-Type": "application/json",
       Authorization: `Bearer ${openai_api_key ?? ""}`,
