bee7a62eaa8728de69476b63bd34a216659066ed
zjzj1996 <guaguaguaxia@Gmail.com>
2024-05-26 22:19:45 +0800

    feat:xiaobaot

--- components/Header.tsx
+++ components/Header.tsx
@@ -23,6 +23,18 @@ export default function Header() {
           {t('title')}
         </h1>
       </Link>
+      <div className="flex items-center right">
+      <span className="text-lg text-black-900">海量精选优质内容，快来看看！&nbsp;  &nbsp;</span>
+
+      <Image
+          alt="header text"
+          src="/finger.svg"
+          className="sm:w-12 sm:h-12 w-1 h-1 right"
+          width={32}
+          height={32}
+        />
+    
+        </div>
       <div className="flex gap-2">
       {
         otherLocale && (
@@ -34,7 +46,7 @@ export default function Header() {
           </div>
         )
       }
-    <Link href="https://www.xiaobaotong.vip/" className="flex space-x-3" legacyBehavior>
+    <Link href="https://www.xiaobaotong.vip/" className="flex space-x-3" legacyBehavior >
       <a target="_blank" rel="noopener noreferrer">
         <Image
               alt="header text"
@@ -45,7 +57,7 @@ export default function Header() {
             />
       </a>
     </Link>
-      </div>
+    </div>
 
 
     </header>
