752a522058c80b0da8ad2175964324859b54b613
Joey <guaguaguaxia@Gmail.com>
2026-07-28 13:48:06 +0800

    1

--- components/Header.tsx
+++ components/Header.tsx
@@ -23,41 +23,8 @@ export default function Header() {
           {t('title')}
         </h1>
       </Link>
-      <div className="flex items-center right">
-      <span className="text-lg text-black-900">海量精选优质内容，快来看看！&nbsp;  &nbsp;</span>
 
-      <Image
-          alt="header text"
-          src="/finger.svg"
-          className="sm:w-12 sm:h-12 w-8 h-8 right"
-          width={32}
-          height={32}
-        />
-    
-        </div>
-      <div className="flex gap-2">
-      {
-        otherLocale && (
-          <div
-            className="relative font-medium text-black-600 before:absolute before:-bottom-1 before:h-0.5 before:w-full before:scale-x-0 before:bg-indigo-600 before:transition hover:before:scale-x-100">
-          <Link href={route} locale={otherLocale}>
-            {t('switchLocale', { locale: otherLocale })}
-          </Link>
-          </div>
-        )
-      }
-    <Link href="https://www.xiaobaotong.vip/" className="flex space-x-3" legacyBehavior >
-      <a target="_blank" rel="noopener noreferrer">
-        <Image
-              alt="header text"
-              src="/logo.svg"
-              className="sm:w-12 sm:h-12 w-8 h-8"
-              width={32}
-              height={32}
-            />
-      </a>
-    </Link>
-    </div>
+
 
 
     </header>
